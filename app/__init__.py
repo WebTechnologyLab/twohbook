c84ca2c35e58c678d15ab4756d66408475bab989
Anurag Pandey <anuragcarbon1998@gmail.com>
2020-04-16 14:10:31 +0530

    facelifted

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
-
+from flask_moment import Moment
 app = Flask(__name__)
 app.config.from_object(Config)
 
@@ -13,5 +13,6 @@ migrate = Migrate(app, db)
 bootstrap = Bootstrap(app)
 login = LoginManager(app)
 login.login_view = 'index'
+moment = Moment(app)
 
 from app import routes, models, errors
